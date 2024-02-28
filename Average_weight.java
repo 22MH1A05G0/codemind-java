4900e2fea6956da858ea6f2358eb83ae845ca281
22MH1A05G0 <121595363+22MH1A05G0@users.noreply.github.com>
2024-02-28 12:02:01 +0530

    import java.util.*; public class fun{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         int a = sc.nextInt();         int b = sc.nextInt();         int r = 3*x-(a+b);         System.out.println(r);     } }

---
+++ Average_weight.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class fun{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int r = 3*x-(a+b);
+        System.out.println(r);
+    }
+}
