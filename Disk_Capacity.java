0fa5facfa29340ceba345efcc136b4205da98f1f
22MH1A05G0 <121595363+22MH1A05G0@users.noreply.github.com>
2024-02-28 12:12:22 +0530

    import java.util.*; public class fun{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int t = sc.nextInt();         int s = sc.nextInt();         int b = sc.nextInt();         int r = (2*t*s*b*512);         System.out.println(r);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class fun{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int s = sc.nextInt();
+        int b = sc.nextInt();
+        int r = (2*t*s*b*512);
+        System.out.println(r);
+    }
+}
