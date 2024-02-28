83fc24f6a9125bb3cd45ce7aca63af8bbf71f101
22MH1A05G0 <121595363+22MH1A05G0@users.noreply.github.com>
2024-02-28 11:14:56 +0530

    import java.util.*; public class fun{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int area = a*a;         int perimeter =4*a;         System.out.printf("%d %d",area,perimeter);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class fun{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int area = a*a;
+        int perimeter =4*a;
+        System.out.printf("%d %d",area,perimeter);
+    }
+}
