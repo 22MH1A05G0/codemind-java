0839b8316ba532721582c90e0cca7a4f146a57f8
22MH1A05G0 <121595363+22MH1A05G0@users.noreply.github.com>
2024-02-28 11:31:08 +0530

    import java.util.*; public class fun{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         int y = sc.nextInt();         int r = 2*x - y;         System.out.println(r);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class fun{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int r = 2*x - y;
+        System.out.println(r);
+    }
+}
