c1c830d68fdbf679b92915de71883e6f50a17f07
22MH1A05G0 <121595363+22MH1A05G0@users.noreply.github.com>
2024-02-28 11:25:38 +0530

    import java.util.*; public class fun{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int r = (a*5)+(b*7);         System.out.println(r);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class fun{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int r = (a*5)+(b*7);
+        System.out.println(r);
+    }
+}
