1a1259cf8c48764ff85a97c36783978674ccf360
22MH1A05G0 <121595363+22MH1A05G0@users.noreply.github.com>
2024-02-28 11:39:39 +0530

    import java.util.*; public class fun{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);             double a = sc.nextInt();             double r = (a*5)/18;             System.out.printf("%.2f",r);
        }
    }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class fun{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+            double a = sc.nextInt();
+            double r = (a*5)/18;
+            System.out.printf("%.2f",r);
+        
+    }
+}
