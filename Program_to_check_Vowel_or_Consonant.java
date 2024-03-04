d0097f733ffe1015a5debfeec0e95e0bbdf96337
22MH1A05G0 <121595363+22MH1A05G0@users.noreply.github.com>
2024-03-04 11:41:09 +0530

    import java.util.*; public class fun{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         char s =  sc.next().charAt(0);         if(s =='a' || s =='e' || s =='i' || s =='o' || s =='u'|| s == 'A' || s == 'E' || s == 'I' || s =='O' || s == 'U'){             System.out.println("Vowel");         }         else{             System.out.println("Consonant");         }     } }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class fun{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char s =  sc.next().charAt(0);
+        if(s =='a' || s =='e' || s =='i' || s =='o' || s =='u'|| s == 'A' || s == 'E' || s == 'I' || s =='O' || s == 'U'){
+            System.out.println("Vowel");
+        }
+        else{
+            System.out.println("Consonant");
+        }
+    }
+}
